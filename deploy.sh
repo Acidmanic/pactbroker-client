d0614fc48d0af229e048ed5f2dbf029f40297922
Mani Moayedi <you@example.com>
2021-04-17 00:40:51 +0430

    Fix missing qoutes for jar file address

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cp target/application-standalone.jar $BASE && \
 echo 'DIR=$(dirname $BASH_SOURCE)' > $BASE/run.sh && \
 echo 'DIR=$(realpath $DIR)' >> $BASE/run.sh && \
 echo 'JAR=$DIR/application-standalone.jar' >> $BASE/run.sh && \
-echo 'java -jar $JAR $@' >> $BASE/run.sh && \
+echo 'java -jar "$JAR" $@' >> $BASE/run.sh && \
 chmod +x $BASE/run.sh
 
